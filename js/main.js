d02b5c7f0ae16bee3ebce345c365138f4e77007e
Kyle Hardy <150178359+jarvisx86@users.noreply.github.com>
2026-04-26 11:55:41 +0000

    feat: add audio files to site

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Responsible for creating tune elements and adding them to
 // the tunes list elements
 
-function createTuneElement(title, audioPath) {
+function createTuneElement(audioFile) {
     let newElement = document.createElement("div");
     let html = `
-        <h3 class="new-tune-header">${title}</h3>
+        <h3 class="new-tune-header">${audioFile.name}</h3>
         <audio class="new-tune-audio-track" 
             controls 
-            src="${audioPath}" 
+            src="${audioFile.path}"
             type="audio/mpeg"></audio>`;
     
     newElement.innerHTML = html;
@@ -18,9 +18,37 @@ function createTuneElement(title, audioPath) {
 
 function populateTuneList() {
     let tunes = document.getElementById("tunes");
-    
-    for (var i = 0; i < 5; i++) {
-        let audioElement = createTuneElement(`Tune ${i}`, "")
+    let audioFiles = [{
+            name: "Binz",
+            path: "audio/Binz.mp3"
+        },
+        {
+            name: "Chandelier",
+            path: "audio/Chandelier.mp3"
+        },
+        {
+            name: "Don't Blame Us For The Boogie",
+            path: "audio/DontBlameUsForTheBoogie.mp3"
+        },
+        {
+            name: "Golden",
+            path: "audio/Golden.mp3"
+        },
+        {
+            name: "Lose Yourself to Dance",
+            path: "audio/LoseYourself.mp3"
+        },
+        {
+            name: "Passion Fruit",
+            path: "PassionFruit.mp3"
+        },
+        {
+            name: "Ta Douleur",
+            path: "audio/ta-douleur.mp3"
+        }];
+
+    for (var i = 0; i < audioFiles.length; i++) {
+        let audioElement = createTuneElement(audioFiles[i])
         tunes.appendChild(audioElement);
     }
 }
